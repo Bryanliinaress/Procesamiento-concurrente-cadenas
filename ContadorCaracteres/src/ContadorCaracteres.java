41a8370a4670ee0aa8e4d8af10a40a5f52761964
Bryanliinaress <blingon0901@g.educaand.es>
2025-10-26 12:58:22 +0100

    PasoAyPasoB Completado

---
+++ ContadorCaracteres/src/ContadorCaracteres.java
@@ -0,0 +1,63 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class ContadorCaracteres {
+    public static void main(String[] args) throws Exception {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(isr);
+        // Obtenemos el stream de lectura de la entrada estándar
+        // utilizamos un lector con Buffered, para no perder ningún dato
+
+        System.out.println("Contador de caracteres por línea");
+        System.out.println();
+        System.out.println();
+        OutputStream os = System.out;
+        PrintWriter pw = new PrintWriter(os, true);
+        String lineaTeclado = null;
+        /*Aquí cojo el codigo de CrearMúltiplesAccesos.java para redirigir las salidas al fichero javalog.txt
+        y ver como va funcionando el programa.
+        */
+        // Redirigir salidas a javalog.txt 
+        try {
+            // Redirigimos salida estándar y de error a un fichero
+            PrintStream ps = new PrintStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(new File("javalog.txt"), true)),
+                    true);
+            System.setOut(ps);
+            System.setErr(ps);
+        } catch (Exception e) {
+            System.err.println("Error al redirigir las salidas");
+            System.err.println(e.toString());
+        }
+
+        try {
+            System.out.println("Proceso lector");
+            // Mostramos que el proceso que está escribiendo es el que está
+            // leyendo los datos.
+            while ((lineaTeclado = bf.readLine()) != null) {
+                //Aqui leemos solo las lineas que tienen entre 5 y 15 caracteres
+                if (lineaTeclado.length()>=5 && lineaTeclado.length()<=15) {
+                System.out.println("Número de caracteres leídos: " + lineaTeclado.length());
+                System.out.println(lineaTeclado + "," + lineaTeclado.length());
+                pw.println(lineaTeclado+"," + lineaTeclado.length());
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println("Se ha producido un error de E/S.");
+            System.err.println(ex.toString());
+        }
+
+        System.out.println();
+        System.out.println("Proceso contador de caracteres finalizado.");
+        System.out.println();
+        System.out.println();
+    }
+}
--- GeneradorTextos/src/GeneradorTextos.java
+++ GeneradorTextos/src/GeneradorTextos.java
@@ -1,12 +1,52 @@
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 
 public class GeneradorTextos {
     public static void main(String[] args) throws Exception {
-        OutputStream outStream = System.out;
-        PrintWriter printWriter = new PrintWriter(outStream, true);
+        String abecedario = "abcdefghijklmnopqrstuvwxyz";
+        OutputStream os = System.out;
+        PrintWriter pw = new PrintWriter(os, true);
+        System.out.println("Proceso generador de texto");
+        System.out.println();
+        System.out.println();
+        /*Aquí cojo el codigo de CrearMúltiplesAccesos.java para redirigir las salidas al fichero javalog.txt
+        y ver como va funcionando el programa.
+        */
+        // Redirigir salidas a javalog.txt 
+        try {
+            // Redirigimos salida estándar y de error a un fichero
+            PrintStream ps = new PrintStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(new File("javalog.txt"), true)),
+                    true);
+            System.setOut(ps);
+            System.setErr(ps);
+        } catch (Exception e) {
+            System.err.println("Error al redirigir las salidas");
+            System.err.println(e.toString());
+        }
 
-        // String.CharAT(Sorteo)
+        System.out.println("Generando texto aleatorio...");
+        for (int i = 0; i < 15; i++) {
+            // variable para hacer que el texto sea de minimo 5 caracteres y maximo 15
+            int longitudPalabra = (int) (Math.random() * 11) + 5; // Longitud entre 5 y 15
+            StringBuilder palabraGenerada = new StringBuilder();
+            for (int j = 0; j < longitudPalabra; j++) {
+                int indiceAleatorio = (int) (Math.random() * abecedario.length());
+                char letraAleatoria = abecedario.charAt(indiceAleatorio);
+                palabraGenerada.append(letraAleatoria);
+            }
+            System.out.println(palabraGenerada.toString());
+            pw.println(palabraGenerada.toString());
+        }
+        System.out.println();
+        System.out.println("Textos aleatorios generados.");
+        System.out.println();
+        System.out.println();
     }
-}//lfkjsdalkfjaslkjf
+}
